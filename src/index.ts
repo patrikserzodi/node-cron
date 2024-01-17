e6618505f77f464cbebaefb71e0e3777b99b7c6b
patrikserzodi <serzodip@gmail.com>
2024-01-17 20:46:36 +0100

    removed axios

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import cron from 'node-cron';
-import axios from 'axios';
 
 const urlToFetch = 'https://web-production-6b37.up.railway.app/start_server/';
 
 cron.schedule('0-59/10 * * * *', async () => {
   console.log(`Running the cycle ...`);
-  const response = await axios.get(urlToFetch);
-  console.log(`Fetched data:`, response.data);
+  const response = await fetch(urlToFetch);
+  console.log(`Fetched data:`, response.json());
 });
