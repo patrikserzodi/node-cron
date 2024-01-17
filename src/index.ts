5286ac38cf2d8630a603ac10c57bfe441f505361
patrikserzodi <serzodip@gmail.com>
2024-01-17 20:41:27 +0100

    added cron to fetch url

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import cron from 'node-cron';
+import axios from 'axios';
 
-cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+const urlToFetch = 'https://web-production-6b37.up.railway.app/start_server/';
+
+cron.schedule('0-59/10 * * * *', async () => {
+  console.log(`Running the cycle ...`);
+  const response = await axios.get(urlToFetch);
+  console.log(`Fetched data:`, response.data);
 });
