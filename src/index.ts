c0cdf9e5ac8c321373c39876f5a97229997ad035
patrikserzodi <serzodip@gmail.com>
2024-01-20 13:03:23 +0100

    updated cron to work every 20 min on weekend days and every 10 min od week days

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import cron from 'node-cron';
 
 const urlToFetch = 'https://web-production-6b37.up.railway.app/start_server/';
 
-cron.schedule('0-59/10 * * * *', async () => {
+cron.schedule('0-59/10 * * * 1-5', async () => {
+  console.log(`Running the cycle ...`);
+  const response = await fetch(urlToFetch);
+  console.log(`Fetched data:`, response.json());
+});
+
+
+cron.schedule('0-59/20 * * * 6,0', async () => {
   console.log(`Running the cycle ...`);
   const response = await fetch(urlToFetch);
   console.log(`Fetched data:`, response.json());
